cf62910fdd0521c1ab3d42a75870ec669c2d5172
Matilda Mared <matildamared@live.se>
2022-05-26 08:57:28 +0200

    add basic components

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+	return <Container>Header</Container>;
+};
+
+const Container = styled.header`
+	font-family: "Antonio", sans-serif;
+`;
+
+export default Header;
