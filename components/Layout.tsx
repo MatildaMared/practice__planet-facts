d056e824953a2cdb8d4ca8d3cc9020a32f7afa48
Matilda Mared <matildamared@live.se>
2022-05-26 08:56:43 +0200

    add layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
+
+interface Props {
+	children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+	return (
+		<>
+			<Header />
+			{children}
+		</>
+	);
+};
+
+export default Layout;
