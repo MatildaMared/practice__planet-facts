5f6ce469e2f8e3087b2a50d7d6d7b159520e0448
Matilda Mared <matildamared@live.se>
2022-05-28 20:31:53 +0200

    remove redirect

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const nextConfig = {
 	compiler: {
 		styledComponents: true,
 	},
-	redirects() {
-		return [
-			{
-				source: "/",
-				destination: "/earth",
-				permanent: true,
-			},
-		];
-	},
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/,
