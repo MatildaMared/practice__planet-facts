2e3ec6afda624161df85c606888a87e77ed2345b
Matilda Mared <matildamared@live.se>
2022-06-02 18:58:17 +0200

    add planet data nav and render images based on what choice the user made

--- components/PlanetInfo.tsx
+++ components/PlanetInfo.tsx
@@ -2,29 +2,65 @@ import React, { useState } from "react";
 import { Planet } from "../models/Planet";
 import styled from "styled-components";
 import IconSource from "./../public/assets/icon-source.svg";
+import { useWidth } from "../hooks/useWidth";
+import { Breakpoint } from "../styles/constans";
 
 interface Props {
 	planetData: Planet;
 }
 
+enum Data {
+	Overview = "overview",
+	Surface = "surface",
+	Structure = "structure",
+}
+
 const PlanetInfo = ({ planetData }: Props) => {
-	const [currentData, setCurrentData] = useState("overview");
+	const [currentData, setCurrentData] = useState<Data>(Data.Overview);
+	const windowWidth = useWidth();
 	console.log(planetData);
 	return (
 		<Container>
 			<Nav>
-				<NavItem onClick={() => setCurrentData("overview")}>
+				<NavItem
+					planetName={planetData.name.toLowerCase()}
+					className={currentData === Data.Overview ? "active" : ""}
+					onClick={() => setCurrentData(Data.Overview)}
+				>
 					Overview
 				</NavItem>
-				<NavItem onClick={() => setCurrentData("structure")}>
-					Structure
+				<NavItem
+					planetName={planetData.name.toLowerCase()}
+					className={currentData === Data.Structure ? "active" : ""}
+					onClick={() => setCurrentData(Data.Structure)}
+				>
+					{windowWidth >= Breakpoint.TabletLandscapePx
+						? "Internal Structure"
+						: "Structure"}
 				</NavItem>
-				<NavItem onClick={() => setCurrentData("surface")}>
-					Surface
+				<NavItem
+					planetName={planetData.name.toLowerCase()}
+					className={currentData === Data.Surface ? "active" : ""}
+					onClick={() => setCurrentData(Data.Surface)}
+				>
+					{windowWidth >= Breakpoint.TabletLandscapePx
+						? "Surface Geology"
+						: "Geology"}
 				</NavItem>
 			</Nav>
-			<ImageWrapper>
-				<PlanetImage src={planetData.images.planet} />
+			<ImageWrapper
+				className={currentData === Data.Surface ? "extra-space" : ""}
+			>
+				<PlanetImage
+					src={
+						currentData === Data.Structure
+							? planetData.images.internal
+							: planetData.images.planet
+					}
+				/>
+				{currentData === Data.Surface && (
+					<GeologyImage src={planetData.images.geology} />
+				)}
 			</ImageWrapper>
 			<Heading>{planetData.name}</Heading>
 			<Text>
@@ -58,14 +94,26 @@ const Nav = styled.nav`
 	justify-content: space-around;
 `;
 
-const NavItem = styled.button`
+interface NavItemProps {
+	planetName: string;
+}
+
+const NavItem = styled.button<NavItemProps>`
 	padding: 20px 0;
 	color: #fff;
 	font-size: 0.875rem;
 	text-transform: uppercase;
 	border: none;
 	background: none;
-	border-bottom: 4px solid red;
+	border-bottom: 4px solid transparent;
+	transition: all 0.2s;
+	cursor: pointer;
+
+	&.active,
+	&:hover {
+		border-bottom: 4px solid
+			${(props) => `var(--color-${props.planetName})`};
+	}
 `;
 
 const Heading = styled.h1`
@@ -79,11 +127,27 @@ const ImageWrapper = styled.figure`
 	padding: 2rem;
 	display: flex;
 	justify-content: center;
+	position: relative;
+	display: flex;
+	flex-direction: column;
+
+	&.extra-space {
+		margin-bottom: 3.5rem;
+	}
 `;
 
 const PlanetImage = styled.img`
 	width: auto;
 	height: auto;
+`;
+
+const GeologyImage = styled.img`
+	position: absolute;
+	width: 40%;
+	height: auto;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, 30%);
 `;
 
 const Text = styled.p`
